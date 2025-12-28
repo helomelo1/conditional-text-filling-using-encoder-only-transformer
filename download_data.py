fdbefccfecf26eb27e08ebf6180eff62e450be8e
helomelo1 <b23ee1069@iitj.ac.in>
2025-12-28 10:41:24 +0530

    minor changes

--- download_data.py
+++ download_data.py
@@ -3,15 +3,19 @@ import numpy as np
 from pathlib import Path
 
 
-def download_data(ticker, start, end, save_path="."):
-    data = yf.download(ticker, start=start, end=end, progress=False)
+tickers = ["^GSPC", "BTC-USD"]
 
-    prices = data[['Close']].dropna()
+def download_data(ticker="^GSPC", start="2020-01-01", end=None, save_path=""):
+    for ticker in tickers:
+        data = yf.download(ticker, start=start, end=end, progress=False)
 
-    Path(save_path).parent.mkdir(parents=True, exist_ok=True)
-    prices.to_csv(save_path)
+        prices = data[['Close']].dropna()
+        save_path = f"{ticker}.csv"
 
-    print(f"Saved {len(prices)} rows to {save_path}")
+        Path(save_path).parent.mkdir(parents=True, exist_ok=True)
+        prices.to_csv(save_path)
+
+        print(f"Saved {len(prices)} rows to {save_path}")
 
 if __name__ == "__main__":
     download_data()
